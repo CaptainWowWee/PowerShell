802e250ad462e5844deb228cf06d3070aa132b65
CaptainWowWee <stephen_pastoriza@hotmail.com>
2021-02-26 09:44:17 +0000

    Add files via upload

---
+++ Connect to Bastions.ps1
@@ -0,0 +1,25 @@
+﻿# Connection Legacy Bastion Hosts
+
+$AUS = "10.49.228.10"
+$EURO = "10.49.212.10"
+$SING = "10.49.214.10" #IMCP Traffic blocked
+$USA = "10.49.213.10"
+
+
+# Continuous Ping using Test Connection
+while (1) {
+   Test-Connection -ComputerName $AUS
+}  
+
+
+# Continous Ping
+ping -t $Sing
+
+# Australia - Detailed Ping includes Ping status
+Test-NetConnection -ComputerName $AUS -InformationLevel "Detailed"
+
+# Europe West - Detailed Ping includes Ping status
+Test-NetConnection -ComputerName $EURO -InformationLevel "Detailed"
+
+# USA - Detailed Ping includes Ping status
+Test-NetConnection -ComputerName $USA -InformationLevel "Detailed"
